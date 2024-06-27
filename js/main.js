876a7746b0f1f330cd4a57298ca5029561764887
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-27 18:22:37 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ createApp({
   data() {
     return {
       EmailList: [],
-      Email: "",
     };
   },
   methods: {},
@@ -14,16 +13,14 @@ for (let index = 0; index < 10; index++) {
       .get("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((email) => {
         const result = email.data;
-        console.log( "1"+ result);
-        this.Email = result.response;
-         console.log("2"+this.Email);
-        this.EmailList.push(this.Email);
+       
+        this.EmailList.push(result.response);
        
         console.log( "3"+this.EmailList);
       }); 
-    
 }
     
 
   },
 }).mount("#container");
+
